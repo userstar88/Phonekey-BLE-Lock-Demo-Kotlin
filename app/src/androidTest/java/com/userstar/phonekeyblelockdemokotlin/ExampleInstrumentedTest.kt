bd040416b08cc493a568a0fec962d730a9057eae
yhw <cool890104@yahoo.com.tw>
2020-08-27 15:17:20 +0800

    [Debug]

--- app/src/androidTest/java/com/userstar/phonekeyblelockdemokotlin/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/userstar/phonekeyblelockdemokotlin/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.userstar.phonekeybasicfunctiondemokotlin
+package com.userstar.phonekeyblelockdemokotlin
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.userstar.phonekeybasicfunctiondemokotlin", appContext.packageName)
+        assertEquals("com.userstar.phonekeyblelockdemokotlin", appContext.packageName)
     }
 }
